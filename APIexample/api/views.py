80ffb270171f31b221a3fbecfefd95ce5dfbca9f
Damon <damonlhill82@gmail.com>
2022-06-08 12:53:00 +1000

    Added user viewset

--- APIexample/api/views.py
+++ APIexample/api/views.py
@@ -13,7 +13,7 @@ class ArticleViewSet(viewsets.ModelViewSet):
 
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
-	serializer_class = UserSerializer()
+	serializer_class = UserSerializer
 
 
 '''
