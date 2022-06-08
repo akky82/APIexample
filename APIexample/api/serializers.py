691f3818b71ffd6c894137277900daac0eca7889
Damon <damonlhill82@gmail.com>
2022-06-08 13:01:31 +1000

    Added password hashing and dynamic token creation on user registration

--- APIexample/api/serializers.py
+++ APIexample/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Article
 from django.contrib.auth.models import User
+from rest_framework.authtoken.views import Token
 
 
 """
@@ -27,3 +28,12 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['id', 'username', 'password']
+		extra_kwargs = {'password': {
+			'write_only': True,
+			'required': True,
+		}}
+
+	def create(self, validated_data):
+		user = User.objects.create_user(**validated_data)
+		Token.objects.create(user=user)
+		return user
